93670527dda77a7b5f74c220204ea3b2cbf78122
Dev-14112002 <gulatisaksham2002@gmail.com>
2026-04-19 11:31:25 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Saksham Gulati",
+    author_email="gulatisaksham1234@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
